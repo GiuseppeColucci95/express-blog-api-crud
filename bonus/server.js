0c821c6443c97eb188c9dac519a7110a348b6574
GiuseppeColucci95 <colucci539@gmail.com>
2025-03-14 16:00:43 +0100

    add bonus folder and copy all files

---
+++ bonus/server.js
@@ -0,0 +1,22 @@
+//import express library
+const express = require('express');
+//create an instance of express
+const app = express();
+//create server port
+const port = 3000;
+
+//import posts router
+const postsRouter = require('./routers/postsRouter');
+
+//start server listening
+app.listen(port, () => {
+  console.log(`🟢 Server start running on port http://localhost:${port}`);
+});
+
+//home route
+app.get('/', (req, res) => {
+  res.send("Welcome to our Blog Server!");
+});
+
+//posts middleware
+app.use('/posts', postsRouter);
